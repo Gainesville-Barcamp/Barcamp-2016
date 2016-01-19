661e6740c402c70cf3c4b1f8e0f160f816109d36
Mike Herchel <mike@herchel.com>
2016-01-18 20:50:14 -0500

    Debugging

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ var navIcon = document.querySelector('.nav-menu-icon'),
 
     console.log(navIcon);
 
-function addClass() {
-  mapcover.className += ' js-selected';
-}
+// function addClass() {
+//   mapcover.className += ' js-selected';
+// }
 
-function toggleNav() {
-  navIcon.classList.toggle('js-nav-active');
-  navMenu.classList.toggle('js-nav-active');
-  overlay.classList.toggle('js-active');
-}
+// function toggleNav() {
+//   navIcon.classList.toggle('js-nav-active');
+//   navMenu.classList.toggle('js-nav-active');
+//   overlay.classList.toggle('js-active');
+// }
 
-window.onload = function() {
-  if (mapcover) {
-    mapcover.addEventListener( 'click' , addClass );
-  }
-  if (navIcon) {
-    navIcon.addEventListener('click', toggleNav);
-    navIcon.addEventListener('touchstart', toggleNav);
-  }
-  // Make sure the nav disappears when jumping to an anchor
-  if (anchorLink) {
-    anchorLink.addEventListener('click', toggleNav);
-  }
-};
+// window.onload = function() {
+//   if (mapcover) {
+//     mapcover.addEventListener( 'click' , addClass );
+//   }
+//   if (navIcon) {
+//     navIcon.addEventListener('click', toggleNav);
+//     navIcon.addEventListener('touchstart', toggleNav);
+//   }
+//   // Make sure the nav disappears when jumping to an anchor
+//   if (anchorLink) {
+//     anchorLink.addEventListener('click', toggleNav);
+//   }
+// };
